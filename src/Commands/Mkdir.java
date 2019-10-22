9e05fdd8cdc0cb9af6528919a471d89ae1a7ef81
OVGB <44819887+OVGB@users.noreply.github.com>
2019-10-22 22:56:48 +0200

    Create Mkdir.java

---
+++ src/Commands/Mkdir.java
@@ -0,0 +1,31 @@
+package Commands;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Mkdir extends Command {
+    public Mkdir() {
+        argNum = 1;
+    }
+
+    @Override
+    public String run(String[] args, String args1){
+        if(!(new File(args[0]).exists())) {
+            try {
+                Files.createDirectories(Paths.get(args[0]));
+
+            } catch (IOException ex) {
+                return "File Creation Failed";
+            }
+            ;
+
+            return "";
+        }
+        else System.out.println("File Already Exists");
+
+        return "";
+    }
+}
