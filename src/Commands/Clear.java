bf430c5cf8242e7e2c152e2fcf5ac67afb59a0af
Youssef Hammad <32943220+Youssef-Hammad@users.noreply.github.com>
2019-10-23 00:50:40 +0200

    Create Clear.java

---
+++ src/Commands/Clear.java
@@ -0,0 +1,10 @@
+public class Clear extends Command{
+    public Clear() { argNum = 0; }
+    @Override
+    public void run() {
+        System.out.println("clear running");
+        for(int i = 0; i < 100; i++) {
+            System.out.print("\n");
+        }
+    }
+}
