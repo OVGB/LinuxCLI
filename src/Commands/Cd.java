9e39dd0241b591c3b23a54695b5cb7fd3b413ba0
Youssef Hammad <32943220+Youssef-Hammad@users.noreply.github.com>
2019-10-24 02:09:14 +0200

    Created cd, cd .., and cd PATH
    PATH includes handling short paths (cd Users) and full ones (cd C:\Users).
    Also replaces all the / with \ in the path

--- src/Commands/Cd.java
+++ src/Commands/Cd.java
@@ -1,10 +1,53 @@
 package Commands;
 
+import java.io.File;
+
 public class Cd extends Command {
     public Cd() { argNum = 1; }
     @Override
-   public String run(String[] args, String args1){
+    public String run(String[] args, String args1) {
         System.out.println("Cd running");
+        String temppath = args[1]; //input path
+        String path=temppath;  //full path
+
+
+
+        if(temppath.length()==0) {
+            Main.workingDirectory="C:\\";
+            return "";
+        }
+        else if(temppath.equals("..")) {
+            System.out.println(temppath);
+            int stoppingIdx=-1;
+            for(int i = Main.workingDirectory.length()-1; i>=0; i--) {
+                if(Main.workingDirectory.charAt(i)=='\\') { //last \ in the path
+                    stoppingIdx=i;
+                    break;
+                }
+            }
+            if(stoppingIdx!=-1) {
+                String newPath = ""; //newPath after going back
+                for(int i = 0; i <= stoppingIdx; i++) {
+                    newPath+=Main.workingDirectory.charAt(i);
+                }
+                Main.workingDirectory=newPath;
+            }
+            System.out.println(Main.workingDirectory);
+            return "";
+        }
+
+        if(!temppath.contains(":")) {
+            path = Main.workingDirectory+temppath;
+        }
+        path.replaceAll("/","\\");
+        if(new File(path).exists()) {
+            Main.workingDirectory=path+"\\";
+            System.out.println("SUCCESS");
+        }
+        else {
+            System.out.println("error: No such file or directory");
+        }
+
         return "";
     }
 }
